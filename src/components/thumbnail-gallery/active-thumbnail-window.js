6796c2ebb50b9628162120feab1e124e146c9a64
Jacob Cavazos <jacobcavazos@Jacobs-MacBook-Air.local>
2018-10-21 14:01:44 -0700

    created black box where main picture will appear and removed text pre grid

--- src/components/thumbnail-gallery/active-thumbnail-window.js
+++ src/components/thumbnail-gallery/active-thumbnail-window.js
@@ -2,10 +2,15 @@ import React from 'react'
 
 const ActiveThumbnailWindow = () => {
     return (
-        <div>
-          Active Thumbnail
+        <div style={styles}>
+          
         </div>
     )
 }
 
+const styles = {
+    height: '65%',
+    width: '100%',
+    background: '#333'
+}
 export default ActiveThumbnailWindow
